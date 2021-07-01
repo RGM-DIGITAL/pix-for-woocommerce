edd2668f99a6c5df53349a9e96c5beb33b751ea9
rafaelmatostj <rafaelmatostj@gmail.com>
2021-07-01 18:13:28 -0300

    init

---
+++ pix-for-woocommerce.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Plugin Name:Pix for WooCommerce
+ * Plugin URI: https://github.com/rafaelmatostj/pix-for-woocommerce
+ * Description: Receba pagamentos com o  Pix.
+ * Author: Rafael Matos
+ * Author URI: https://github.com/rafaelmatostj
+ * Version: 0.0.0
+ * License: GNU General Public License v3.0
+ *
+ * @package Pix_For_WooCommerce
+ */
+
+defined('ABSPATH') or exit;
+
