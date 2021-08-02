f9d9c1426b6b34ddcf0dc94e6334c3bc3d16695c
rafaelmatostj <rafaelmatostj@gmail.com>
2021-08-02 18:00:29 -0300

    chamando includes

--- pix-for-woocommerce.php
+++ pix-for-woocommerce.php
@@ -10,6 +10,17 @@
  * License: GNU General Public License v3.0*
  *
  */
+defined('ABSPATH') or exit;
+
+define('WC_PIX_VERSION', '1.3.2');
+define('WC_PIX_PLUGIN_FILE', __FILE__);
+define('WC_PIX_PLUGIN_URL', plugin_dir_url(__FILE__));
+define('WC_PIX_PLUGIN_PATH', plugin_dir_path(__FILE__));
+
+if (!class_exists('WC_Pix')) {
+	include_once dirname(__FILE__) . '/includes/class-wc-pix.php';
+	add_action('plugins_loaded', array('WC_Pix', 'init'));
+}
 
 
 
